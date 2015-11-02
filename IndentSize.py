e4d572aaaeca808f85176d3d31a4e5a33292ab3a
German M. Bravo <german.mb@deipi.com>
2015-11-02 10:15:38 -0600

    Fixed indent, unindent and backspace

--- IndentSize.py
+++ IndentSize.py
@@ -3,72 +3,95 @@ import sublime_plugin
 
 
 class IndentSizeCommand(sublime_plugin.TextCommand):
-    def indent(self, edit, region, start_point):
-        translate_tabs_to_spaces = self.view.settings().get("translate_tabs_to_spaces")
+    def indent(self, edit, line, start):
+        start_point = start.begin()
+
         tab_size = self.view.settings().get("tab_size")
-        indent_size = self.view.settings().get("indent_size", tab_size)
+        indent_size = self.view.settings().get("indent_size", tab_size // 2)
         if indent_size > tab_size:
             indent_size = tab_size
 
-        if translate_tabs_to_spaces or indent_size < tab_size:
-            # insert spaces upto the next "indent stop" (like tab stop)
-            row, char_column = self.view.rowcol(start_point)
-            real_column = 0
-            for i in range(start_point - char_column, start_point):
-                if self.view.substr(i) == "\t":
-                    real_column += tab_size
-                else:
-                    real_column += 1
-            space_count = indent_size - (real_column % indent_size)
-            tab = " " * space_count
-        else:
-            tab = "\t"
-        self.view.insert(edit, start_point, tab)
+        indentation_region = sublime.Region(line.begin(), start_point)
+        indentation = self.view.substr(indentation_region)
 
-        if not translate_tabs_to_spaces:
-            start_point += len(tab)
-            line = self.view.line(start_point)
-            indentation_region = sublime.Region(line.begin(), start_point)
-            indentation = self.view.substr(indentation_region)
-            indentation = indentation.replace("\t", " " * tab_size)
-            indentation = indentation.replace(" " * tab_size, "\t")
-            self.view.replace(edit, indentation_region, indentation)
+        # Figure out the true length of the line (in spaces)
+        indentation_length = indentation.replace(" " * tab_size, "\t")
+        indentation_length = indentation_length.replace(" \t", "\t").replace("\t", " " * tab_size)
+        indentation_length = len(indentation_length)
+
+        # How many trailing spaces are there in the line:
+        trailing_spaces = len(indentation) - len(indentation.rstrip(" "))
+
+        # How many characters are there from the last indent stop:
+        indent_mod = indentation_length % indent_size
+
+        # How many spaces are there from the last tab stop:
+        trailing_tab = min(indentation_length % tab_size, trailing_spaces)
+
+        if trailing_tab >= indent_size:
+            trailing = trailing_tab
+            tab = "\t"
+        else:
+            trailing = 0
+            tab = " " * (indent_size - indent_mod)
+
+        if trailing:
+            indentation_region = sublime.Region(start_point - trailing, start_point)
+            self.view.erase(edit, indentation_region)
+        self.view.insert(edit, start_point - trailing, tab)
 
     def run(self, edit):
         for region in reversed(self.view.sel()):
-            for line in reversed(self.view.lines(region)):
-                if line.a != line.b:
-                    start = self.view.find("[^ \t]", line.begin())
-                    if start is None:
-                        start = line
-                    self.indent(edit, region, start.begin())
+            if region.empty():
+                line = self.view.line(region)
+                self.indent(edit, line, region)
+            else:
+                for line in reversed(self.view.lines(region)):
+                    if line.a != line.b:
+                        start = self.view.find("[^ \t]", line.begin())
+                        if start is None:
+                            start = line
+                        self.indent(edit, line, start)
 
 
 class UnindentSizeCommand(sublime_plugin.TextCommand):
-    def unindent(self, edit, region, start_point):
-        translate_tabs_to_spaces = self.view.settings().get("translate_tabs_to_spaces")
+    def unindent(self, edit, line, start):
+        start_point = start.begin()
+
         tab_size = self.view.settings().get("tab_size")
-        indent_size = self.view.settings().get("indent_size", tab_size)
+        indent_size = self.view.settings().get("indent_size", tab_size // 2)
         if indent_size > tab_size:
             indent_size = tab_size
 
-        line = self.view.line(start_point)
         indentation_region = sublime.Region(line.begin(), start_point)
         indentation = self.view.substr(indentation_region)
-        indentation = indentation.replace("\t", " " * tab_size)
-        indentation = indentation[:-indent_size]
-        if not translate_tabs_to_spaces:
-            indentation = indentation.replace(" " * tab_size, "\t")
-        self.view.replace(edit, indentation_region, indentation)
+
+        trailing = 0
+        while True:
+            trailing += 1
+            indentation = indentation[:-1]
+            indentation_length = indentation.replace(" " * tab_size, "\t")
+            indentation_length = indentation_length.replace(" \t", "\t").replace("\t", " " * tab_size)
+            indentation_length = len(indentation_length)
+            if not (indentation_length % indent_size):
+                break
+
+        if trailing:
+            indentation_region = sublime.Region(start_point - trailing, start_point)
+            self.view.erase(edit, indentation_region)
 
     def run(self, edit):
         for region in reversed(self.view.sel()):
-            for line in reversed(self.view.lines(region)):
-                if line.a != line.b:
-                    start = self.view.find("[^ \t]", line.begin())
-                    if start is None:
-                        start = line
-                    self.unindent(edit, region, start.begin())
+            if region.empty():
+                line = self.view.line(region)
+                self.unindent(edit, line, region)
+            else:
+                for line in reversed(self.view.lines(region)):
+                    if line.a != line.b:
+                        start = self.view.find("[^ \t]", line.begin())
+                        if start is None:
+                            start = line
+                        self.unindent(edit, line, start)
 
 
 class BackspaceSizeCommand(UnindentSizeCommand):
@@ -83,7 +106,7 @@ class BackspaceSizeCommand(UnindentSizeCommand):
                         if region.begin() > start.begin():
                             self.view.erase(edit, sublime.Region(region.begin() - 1, region.end()))
                         else:
-                            self.unindent(edit, region, region.begin())
+                            self.unindent(edit, line, region)
                     else:
                         self.view.erase(edit, sublime.Region(region.begin() - 1, region.end()))
             else:
